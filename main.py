66756869857fe9b4315520e76237faa8f3fcc3d4
Hernande Monteiro <monteiro1998@live.com>
2023-06-08 16:02:32 -0300

    build: add gitignore and remove cache

--- main.py
+++ main.py
@@ -1,21 +1,29 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, jsonify
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    return render_template("hello.html")
+    return render_template("hello.html", name="Hernande Monteiro")
 
 
 @app.get("/project_infos")
 def getFunction():
-    return {
+    # body of request
+    req = request.form
+    # to return a json list you need call the jsonify function
+    return jsonify([{
         "author": "Hernande Monteiro",
         "Framework": "Flask",
-        "name": "flask_web_and_api"
-    }
+        "name": "flask_web_and_api",
+        "request": req
+    },
+        {
+        "developer": "Hernande Monteiro",
+        "Framework-Version": "2.0.2",
+    }])
 
 
 if (__name__ == "__main__"):
-    app.run()
+    app.run(debug=True)
