d3265d3a3339e3cf3f523bdd4efd069f1e9099d3
Hernande Monteiro <monteiro1998@live.com>
2023-06-10 20:57:35 -0300

    config: ansible and terraform config

--- terraform_flask_run/main.tf
+++ terraform_flask_run/main.tf
@@ -17,14 +17,6 @@ resource "aws_instance" "hm_solution_server" {
   ami           = "ami-03f65b8614a860c29"
   instance_type = "t2.micro"
   key_name      = "flask-terraform"
-  user_data     = <<-EOF
-                      #!/bin/bash
-                      cd /home/ubuntu
-                      echo “<h1>Mensagem a ser mostrada</h1>” > index.html
-                      nohup busybox httpd -f -p 8080 &
-                      mkdir hellodir
-                      sudo git clone https://github.com/hernandemonteiro/flask_web_and_api
-                      EOF
   tags = {
     Name = "flask_runner"
   }
