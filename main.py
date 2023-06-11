363815ceab2ffe7e675d2b74025f6f7d14a53493
Hernande Monteiro <monteiro1998@live.com>
2023-06-11 00:14:14 -0300

    refactor: clean main.py

--- main.py
+++ main.py
@@ -1,30 +1,19 @@
-from flask import Flask, render_template, request, jsonify, redirect
+from flask import Flask, render_template, jsonify, redirect
 
 app = Flask(__name__)
 
 
 @app.route("/")
 def hello():
-    # and to acess in the html the variable we need {{...}}, but
-    # in case of iteration
-    # likes for we needs {%...%}
     return render_template("hello.html", name="Hernande Monteiro")
 
 
 @app.get("/project_infos")
 def getFunction():
-    req_body = request.form
-
-    # to return a json list you need call the jsonify function
     return jsonify([{
-        "author": "Hernande Monteiro",
+        "Author": "Hernande Monteiro",
         "Framework": "Flask",
-        "name": "flask_web_and_api",
-        "request": req_body
-    },
-        {
-        "developer": "Hernande Monteiro",
-        "Framework-Version": "2.0.2",
+        "repository_name": "flask_web_and_api",
     }])
 
 
